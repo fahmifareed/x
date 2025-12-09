efc8fdd634ab3e0d577689e239848a471e2c4c6d
Div627 <54497462+Div627@users.noreply.github.com>
2025-12-09 18:55:45 +0800

    fix: relying on mermaid to fix umi MFSU checks (#1475)
    * fix: dependece mermaid to fix umi mfsu checking
    
    * fix: fix cr
    
    * fix: fix cr
    
    * fix: fix cr
    
    * fix: add test case
    
    ---------
    
    Co-authored-by: Mickey <951203214@qq.com>

--- packages/x/components/mermaid/Mermaid.tsx
+++ packages/x/components/mermaid/Mermaid.tsx
@@ -2,6 +2,7 @@ import { DownloadOutlined, ZoomInOutlined, ZoomOutOutlined } from '@ant-design/i
 import { Button, Segmented, Tooltip } from 'antd';
 import classnames from 'classnames';
 import throttle from 'lodash.throttle';
+import mermaid from 'mermaid';
 import React, { useEffect, useRef, useState } from 'react';
 import SyntaxHighlighter from 'react-syntax-highlighter';
 import useXComponentConfig from '../_util/hooks/use-x-component-config';
@@ -52,8 +53,16 @@ const Mermaid: React.FC<MermaidProps> = React.memo((props) => {
   const [isDragging, setIsDragging] = useState(false);
   const [lastMousePos, setLastMousePos] = useState({ x: 0, y: 0 });
   const containerRef = useRef<HTMLDivElement>(null);
-  const mermaidRef = useRef<any>(null);
   const id = `mermaid-${uuid++}-${children?.length || 0}`;
+
+  useEffect(() => {
+    mermaid.initialize({
+      startOnLoad: false,
+      securityLevel: 'strict',
+      theme: 'default',
+      fontFamily: 'monospace',
+    });
+  }, []);
 
   // ============================ locale ============================
   const [contextLocale] = useLocale('Mermaid', locale_EN.Mermaid);
@@ -85,24 +94,11 @@ const Mermaid: React.FC<MermaidProps> = React.memo((props) => {
     if (!children || !containerRef.current || renderType === RenderType.Code) return;
 
     try {
-      if (!mermaidRef.current) {
-        const mermaidModule = await import('mermaid');
-        mermaidRef.current = mermaidModule.default;
-        mermaidRef.current.initialize({
-          startOnLoad: false,
-          securityLevel: 'strict',
-          theme: 'default',
-          fontFamily: 'monospace',
-        });
-      }
-
-      const isValid = await mermaidRef.current.parse(children, { suppressErrors: true });
+      const isValid = await mermaid.parse(children, { suppressErrors: true });
       if (!isValid) throw new Error('Invalid Mermaid syntax');
 
       const newText = children.replace(/[`\s]+$/g, '');
-
-      const { svg } = await mermaidRef.current.render(id, newText, containerRef.current);
-
+      const { svg } = await mermaid.render(id, newText, containerRef.current);
       containerRef.current.innerHTML = svg;
     } catch (error) {
       console.warn(`Mermaid render failed: ${error}`);
